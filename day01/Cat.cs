0861a58ae0afa71f2f678b0fdcb9d0fc200e9035
irfanm <rachmatirfanm@gmail.com>
2023-10-09 17:06:18 +0700

    initial commit

---
+++ day01/Cat.cs
@@ -0,0 +1,20 @@
+class Cat
+
+{
+	public string colour;
+	
+	public int leg;
+	public bool isTailLong;
+	public int weight;
+	public int height;
+	
+	public void Jump(){
+		Console.WriteLine("Cat Jump");
+	}
+	
+	public void Run(){
+		Console.WriteLine("Cat Run");
+	}
+
+
+}

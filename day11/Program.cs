fdbae098ad22a79e31c0ee7b68a2d6210105b887
irfanm <rachmatirfanm@gmail.com>
2023-10-27 16:33:08 +0700

    day11

---
+++ day11/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+	static void Main()
+	{
+		
+		"stack".Dump();
+		Stack<int> myStack = new();
+		myStack.Push(3);
+		myStack.Push(7);
+		myStack.Push(9);
+		myStack.Push(10);
+		myStack.Push(7);
+		myStack.Push(4);
+		
+		myStack.Peek().Dump();
+		myStack.Pop().Dump();
+		myStack.Pop().Dump();
+		
+		foreach(var e in myStack)
+		{
+			e.Dump();
+		}
+		
+		"Queue".Dump();
+		Queue<int> myQ = new();
+		myQ.Enqueue(1);
+		myQ.Enqueue(2);
+		myQ.Enqueue(3);
+		
+		myQ.Peek();
+		
+		myQ.Dequeue();
+		foreach(var e in myQ)
+		{
+			e.Dump();
+		}
+	}
+}
+
+static class MyExtension
+{
+	public static void Dump(this object obj)
+	{
+		Console.WriteLine(obj);
+	}
+}
